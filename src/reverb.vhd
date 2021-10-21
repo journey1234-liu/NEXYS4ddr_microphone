757ad47c6661db069c9f22e8b2a15ab0cc35c314
journey1234-liu <65387573+journey1234-liu@users.noreply.github.com>
2021-10-21 08:24:57 +0800

    Update reverb.vhd with translated comments

--- src/reverb.vhd
+++ src/reverb.vhd
@@ -6,8 +6,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity reverb is
-generic( t_reverb : integer := 1000 - 2); -- retard de la réverbération (taile du registre à décalage) -- il vaut mieux mettre une valeur faible pour les simulations
-                                          -- moins 2 car on "perd" 2 étapes à calculer 
+generic( t_reverb : integer := 1000 - 2); -- reverberation delay (offset register cut) -- it is better to put a low value for simulations
+                                          -- minus 2 because we "lose" 2 steps to calculate
 port (
     CLK100MHZ       : in  std_logic;
     CPU_RESETN      : in  boolean;  
@@ -28,17 +28,17 @@ architecture rtl of reverb is
     
     signal r_add_st1    :  signed(17 downto 0);
     
-    signal ech_out_reg    :  signed(18 downto 0); -- plus grand pour pas déborder
+    signal ech_out_reg    :  signed(18 downto 0); -- larger to not overflow
 
 begin
 
-    p_input : process (CLK100MHZ) -- gestion du registre à décalage
+    p_input : process (CLK100MHZ) -- lag registry management
     begin
         if rising_edge(CLK100MHZ) then
             
             if (clk_ech) then
-                p_data(TO_INTEGER(p_ecr)) <= ech_in; -- insertion de la nouvelle valeure
-                r_add_st1 <= p_data(TO_INTEGER(p_lec)); -- buffer de l'échantillon à ajouter
+                p_data(TO_INTEGER(p_ecr)) <= ech_in; -- inserting the new value
+                r_add_st1 <= p_data(TO_INTEGER(p_lec)); -- buffer of the sample to be added
                 
                 if (p_ecr < (t_reverb-2)) then
                     p_ecr <= p_ecr + 1;
@@ -53,7 +53,7 @@ begin
             end if;
                 
             if CPU_RESETN then
-                --p_data <= (others=>(others=> '0' )); -- il ne faut pas essayer de faire de reset sur de la ram !
+                --p_data <= (others=>(others=> '0' )); -- do not try to reset on ram!
                 r_add_st1 <= to_signed (0, r_add_st1'length);
                 p_ecr <= to_unsigned (0,p_ecr'length);
                 p_lec <= to_unsigned (1,p_ecr'length);
@@ -61,7 +61,7 @@ begin
         end if;
     end process p_input;
     
-    p_add_st0 : process (CLK100MHZ) -- addition de l'échantillons enregistré avec l'échantillons rentrant
+    p_add_st0 : process (CLK100MHZ) -- addition of the recorded sample with the re-entering sample
     begin
         if rising_edge(CLK100MHZ) then
             if CPU_RESETN then
@@ -70,13 +70,13 @@ begin
                 
             elsif (clk_ech) then
             
-                ech_out_reg <= resize (ech_in, ech_out_reg'length) + resize (r_add_st1, ech_out_reg'length); -- on perd une étape
+                ech_out_reg <= resize (ech_in, ech_out_reg'length) + resize (r_add_st1, ech_out_reg'length); -- we lose a step
                 
-                if ( ech_out_reg >= to_signed(2**17-1,ech_out_reg'length) ) then -- saturation positive -- on perd une deuxième étape
+                if ( ech_out_reg >= to_signed(2**17-1,ech_out_reg'length) ) then -- positive saturation -- we lose a second step
                     ech_out <= to_signed(2**17-1,ech_out'length);
-                elsif (ech_out_reg <= to_signed(-2**17,ech_out_reg'length)) then -- saturation négative
+                elsif (ech_out_reg <= to_signed(-2**17,ech_out_reg'length)) then -- negative saturation
                     ech_out <= to_signed(-2**17,ech_out'length);
-                else -- si pas de saturation
+                else -- if no saturation
                     ech_out <= resize(ech_out_reg, ech_out'length);
                 end if;
                 
